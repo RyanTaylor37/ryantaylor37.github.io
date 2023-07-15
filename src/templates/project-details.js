c916d92d83b5b9d048a35d6a07bb8b11b99b804f
Ryan Taylor <ryan.c.taylor@vanderbilt.edu>
2023-07-14 23:52:48 -0400

    changing VidHub location

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -10,8 +10,23 @@ const { colors, fontSizes, fonts, navDelay, loaderDelay } = theme;
 import Img from 'gatsby-image';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 //import { Container,Row,Col } from 'react-bootstrap';
+//import AV from "/src/pages/project_page/AutoVehicleSim/av_vid.mp4"
+//import VidHub from "/src/pages/project_page/index.js"
+
 import AV from "/src/pages/project_page/AutoVehicleSim/av_vid.mp4"
-import VidHub from "/src/pages/project_page/VideoHub.js"
+import FT from "/src/pages/project_page/Flamethrower/flamethrower.mp4"
+import ELB from "/src/pages/project_page/ElectricLongboard/e-board.mp4"
+import LT from "/src/pages/project_page/LaserTracker/laser-tracker_demo2.mp4"
+import SMC from "/src/pages/project_page/Solar-Mechanical Charger/smchgr.mp4"
+import VR from "/src/pages/project_page/VanderbiltRobotics/telerun.mp4"
+
+const VidHub = new Map();
+VidHub.set('auto-vehicle-sim', AV); 
+VidHub.set('flame-thrower', FT); 
+VidHub.set('e-longboard', ELB);
+VidHub.set('laser-tracker', LT); 
+VidHub.set('solar-mechanical-charger', SMC);
+VidHub.set('vandy-robotics', VR); 
 
 const StyledMainContainer = styled(Main)``;
 
